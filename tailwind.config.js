301b6e6922ae0bc3bc6a08c3fd34caf016219ef6
km-saifullah <kmsaifullah16@gmail.com>
2023-10-05 10:46:54 +0600

    navbar done!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 tailwind.config = {
   theme: {
     extend: {
+      maxWidth: {
+        container: "1280px",
+      },
       colors: {
-        clifford: "#da373d",
+        primary: "#FFD2A4",
+        navBG: "#0D0D0D",
+        white: "#fff",
+        black: "#1C1D28",
       },
       fontFamily: {
         roboto: "'Roboto Condensed', sans-serif",
