48ae3378f7dff45641c49a556939a836a80f3716
km-saifullah <kmsaifullah16@gmail.com>
2023-10-05 12:42:16 +0600

    Church section done!

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,8 @@ tailwind.config = {
         navBG: "#0D0D0D",
         white: "#fff",
         black: "#1C1D28",
+        subHeading: "#161722",
+        churchItemBG: "#FFF5EB",
       },
       fontFamily: {
         roboto: "'Roboto Condensed', sans-serif",
