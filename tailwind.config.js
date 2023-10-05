96c599e9a28df8fe09955266e56aa9a8d1fd18d2
km-saifullah <kmsaifullah16@gmail.com>
2023-10-05 20:12:50 +0600

    upcoming section done!

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,6 +26,8 @@ tailwind.config = {
         banner: "url('./images/banner.jpg')",
         overlay:
           "linear-gradient(40deg, #060606 16.68%, rgba(0, 0, 0, 0.02) 96.44%)",
+        upcomingText:
+          "linear-gradient(110deg, #A54E2B -53.83%, #DC9853 62.61%)",
       },
     },
   },
